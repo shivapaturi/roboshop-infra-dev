8af6e90a2e45641ac99320203e1c908e30edfeb9
shiva paturi <shivapaturi01@gmail.com>
2025-09-23 23:00:21 +0530

    terraform

--- 10-sg/provider.tf
+++ 10-sg/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "86ss-remote-state-dev"
-    key    = "roboshop-dev-vpc"
+    key    = "roboshop-dev-sg"
     region = "us-east-1"
     encrypt        = true
     use_lockfile = true
