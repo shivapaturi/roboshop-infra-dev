3efa8989f59993587ddb4eee5ae42340aeb92230
shiva paturi <shivapaturi01@gmail.com>
2025-12-25 16:38:19 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -3,7 +3,7 @@ resource "aws_lb_target_group" "catalogue" {
   port     = 8080
   protocol = "HTTP"
   vpc_id   = local.vpc_id
-
+  deregistration_delay = 120
   health_check {
     healthy_threshold = 2
     interval = 5
@@ -57,10 +57,29 @@ resource "terraform_data" "catalogue" {
 resource "aws_ec2_instance_state" "catalogue" {
   instance_id = aws_instance.catalogue.id
   state = "stopped"
-  depends on = [terraform_data.catalogue]
+  depends_on = [terraform_data.catalogue]
 }
 
 resource "aws_ami_from_instance" "catalogue" {
   name               = "${var.project}-${var.environment}-catalogue"
   source_instance_id = aws_instance.catalogue.id
+  depends_on = [aws_ec2_instance_state.catalogue]
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-catalogue"
+    }
+  )
 }
+ 
+ resource "terraform_data" "catalogue_delete" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+
+  # make sure you have aws configure in your laptop
+  provisioner "local-exec" {
+    command = "aws ec2 terminate-instances --instances-ids ${aws_instance.catalogue.id}"
+  }
+  depends_on = [aws_ami_from_instance.catalogue]
+} 
