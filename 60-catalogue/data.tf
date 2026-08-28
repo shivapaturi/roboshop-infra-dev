30a08f53403e045a42c45425ed561ed731d06cae
shiva paturi <shivapaturi01@gmail.com>
2026-08-28 22:57:58 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -1,6 +1,22 @@
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project}/${var.environment}/vpc_id"
+}
+
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project}/${var.environment}/private_subnet_ids"
+}
+
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project}/${var.environment}/catalogue_sg_id"
+}
+
+data "aws_ssm_parameter" "backend_alb_listener_arn" {
+  name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
+}
+
 data "aws_ami" "joindevops" {
-  most_recent = true
   owners      = ["973714476881"]
+  most_recent = true
 
   filter {
     name   = "name"
@@ -17,19 +33,3 @@ data "aws_ami" "joindevops" {
     values = ["hvm"]
   }
 }  
-
-data "aws_ssm_parameter" "catalogue_sg_id" {
-  name = "/${var.project}/${var.environment}/catalogue_sg_id"
-}
-
-data "aws_ssm_parameter" "vpc_id" {
-  name = "/${var.project}/${var.environment}/vpc_id"
-}
-
-data "aws_ssm_parameter" "private_subnet_ids" {
-  name = "/${var.project}/${var.environment}/private_subnet_ids"
-}
-
-data "aws_ssm_parameter" "backend_alb_listener_arn" {
-    name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
-}
--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,13 +1,13 @@
 locals {
-  vpc_id = data.aws_ssm_parameter.vpc_id.value
-  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-  ami_id          = data.aws_ami.joindevops.id
-  private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
-  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
-  backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
-  common_tags = {
-      Project     = var.project         # e.g. "joindevops"
-      Environment = var.environment     # e.g. "dev"
-      Terraform   = "true"
-  }
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    private_subnet_id = split ("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    private_subnet_ids = split ("," , data.aws_ssm_parameter.private_subnet_ids.value)
+    ami_id = data.aws_ami.joindevops.id
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
+    common_tags = {
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
+    }
 }
