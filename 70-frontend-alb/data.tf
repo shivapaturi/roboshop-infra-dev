35089da502eb864a78c31daa2d455cbf132dda72
shiva paturi <shivapaturi01@gmail.com>
2026-08-28 08:25:02 +0530

    terraform

---
+++ 70-frontend-alb/data.tf
@@ -0,0 +1,15 @@
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project}/${var.environment}/vpc_id"
+}
+
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project}/${var.environment}/private_subnet_ids"
+}
+
+data "aws_ssm_parameter" "backend_alb_sg_id" {
+    name = "/${var.project}/${var.environment}/backend_alb_sg_id"
+}
+
+data "aws_ssm_parameter" "acm_certificate_arn" {
+    name = "/${var.project}/${var.environment}/acm_certificate_arn"
+}
