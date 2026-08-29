8499ab8735436d5a7faa5982419f6e886ed7271c
shiva paturi <shivapaturi01@gmail.com>
2026-08-29 21:18:13 +0530

    terraform

---
+++ 60-catalogue/data.tf
@@ -0,0 +1,16 @@
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project}/${var.environment}/vpc_id"
+}
+
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project}/${var.environment}/private_subnet_ids"
+}
+
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project}/${var.environment}/catalogue_sg_id"
+}
+
+data "aws_ssm_parameter" "backend_alb_listener_arn" {
+  name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
+}
+
